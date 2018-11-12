f136472238316cec14de50470639696e947f45f2
Jonatan Flyckt <fljo1589@mace2428-12.jth-stud1.hj.se>
2018-11-12 15:53:01 +0100

    Initial commit

---
+++ CV-Lab2/CV-Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV-Lab2
+//
+//  Created by Jonatan Flyckt on 2018-11-12.
+//  Copyright © 2018 Jonatan Flyckt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
