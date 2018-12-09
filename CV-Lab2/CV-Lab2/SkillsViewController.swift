fd1e35fabda337069c2bfa231a23d6da9f2cf93d
Jonatan Flyckt <fljo1589@mace2428-20.jth-stud1.hj.se>
2018-12-09 22:27:03 +0100

    Adds animation to skills view.

--- CV-Lab2/CV-Lab2/SkillsViewController.swift
+++ CV-Lab2/CV-Lab2/SkillsViewController.swift
@@ -10,19 +10,28 @@ import UIKit
 
 class SkillsViewController: UIViewController {
     
+    @IBOutlet weak var textView: UITextView!
+    
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let press = UITapGestureRecognizer(target: self, action: #selector(tapFunction))
+        imageView.image = UIImage(named: "me")
+        imageView.contentMode = .scaleAspectFit
+        let press = UITapGestureRecognizer(target: self, action: #selector(spinny))
         imageView.addGestureRecognizer(press)
     }
     
     @objc func spinny(sender:UITapGestureRecognizer) {
-        
-        
-        
+        let animate = CABasicAnimation(keyPath: "transform.rotation")
+        animate.duration = 0.4
+        animate.repeatCount = 100
+        animate.fromValue = 0.0
+        animate.toValue = Float(Double.pi * 2.0)
+        imageView.layer.add(animate, forKey: "rotate")
     }
-    
-    
+
 }
+
